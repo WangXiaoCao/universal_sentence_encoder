394e717419ba0f33b32a8417961a63a800b767a0
wasiuva <wua4nw@virginia.edu>
2017-08-03 21:45:10 -0400

    fixed bash files

--- source/data/get_data.sh
+++ source/data/get_data.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-quora='https://drive.google.com/uc?export=download&id=0B8ZGlkqDw7hFU2I1Vkp4NWduSTA'
-glovepath='https://drive.google.com/uc?export=download&id=0B8ZGlkqDw7hFbmdId3JDY0YwY2M'
+quora_fid="0B8ZGlkqDw7hFU2I1Vkp4NWduSTA"
+glove_fid="0B8ZGlkqDw7hFbmdId3JDY0YwY2M"
 
 # download quora dataset split.
 mkdir quora
-curl -Lo quora/quora.zip $quora
+id=`curl -c cookie.txt -s -L "https://drive.google.com/uc?export=download&id=$quora_fid" | grep confirm | sed -e "s/^.*confirm=\(.*\)&amp;id=.*$/\1/"`
+curl -b cookie.txt -L -o quora/quora.zip "https://drive.google.com/uc?confirm=$id&export=download&id=$quora_fid"
+rm cookie.txt
 unzip quora/quora.zip -d quora
 rm quora/quora.zip
 
-
 # download pretrained embeddings only for quora dataset.
 mkdir glove
-curl -Lo glove/glove.zip $glovepath
+id=`curl -c cookie.txt -s -L "https://drive.google.com/uc?export=download&id=$glove_fid" | grep confirm | sed -e "s/^.*confirm=\(.*\)&amp;id=.*$/\1/"`
+curl -b cookie.txt -L -o glove/glove.zip "https://drive.google.com/uc?confirm=$id&export=download&id=$glove_fid"
+rm cookie.txt
 unzip glove/glove.zip -d glove
 rm glove/glove.zip
--- source/trained_models/get_pretrained.sh
+++ source/trained_models/get_pretrained.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-pretrained='https://drive.google.com/uc?export=download&id=0B8ZGlkqDw7hFSm1MQ2FDVTZCTjA'
+fid="0B8ZGlkqDw7hFSm1MQ2FDVTZCTjA"
 
 # download pretrained models.
-curl -o pretrained_models.zip $pretrained
+id=`curl -c cookie.txt -s -L "https://drive.google.com/uc?export=download&id=$fid" | grep confirm | sed -e "s/^.*confirm=\(.*\)&amp;id=.*$/\1/"`
+curl -b cookie.txt -L -o pretrained_models.zip "https://drive.google.com/uc?confirm=$id&export=download&id=$fid"
+rm cookie.txt
 unzip pretrained_models.zip
 rm pretrained_models.zip
